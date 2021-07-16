7991b420c28465f8a9caf2183592f53b229db2a8
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-07-16 16:22:37 +0200

    Initialized common folder

---
+++ common/url.py
@@ -0,0 +1,6 @@
+# Urls videos
+TAIPEI = {"Index": 0, "Name": "TAIPEI", "Lane": 2, "Url": "https://youtu.be/XV1q_2Cl6mI"}  # Taiwan
+BUSAN = {"Index": 1, "Name": "BUSAN", "Lane": 0, "Url": "https://youtu.be/pUcWdJoAuyw"}  # South Korea
+JACKSON_HOLE = {"Index": 2, "Name": "JACKSON HOLE", "Lane": 0, "Url": "https://youtu.be/1EiC9bvVGnk"}  # USA
+DERRY = {"Index": 3, "Name": "DERRY", "Lane": 0, "Url": "https://youtu.be/3g_xTJWPJ74"}  # USA
+CAMBRIDGE = {"Index": 4, "Name": "CAMBRIDGE", "Lane": 0, "Url": "https://youtu.be/f1DyY6a44yA"}  # USA
