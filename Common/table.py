1371870debf8292b0dd857824c69790c04628039
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-08-12 09:18:10 +0200

    Added documentation. Defined function check_cell

--- Common/table.py
+++ Common/table.py
@@ -38,6 +38,13 @@ class Table(QWidget):
         self.show()
 
     def update_table(self, name_vehicle, velocity):
+        r"""
+        Update velocity of the vehicles into table.
+
+        :param name_vehicle: vehicle name.
+        :param velocity: velocity to be updated.
+        """
+
         log(2, "Update row")
 
         columns = self.table.columnCount()
@@ -57,6 +64,12 @@ class Table(QWidget):
                         pass
 
     def delete_row(self, name_vehicle):
+        r"""
+        Delete row in the table.
+
+        :param name_vehicle: name of the vehicle to be deleted.
+        """
+
         log(2, f"Delete row of {name_vehicle}")
 
         columns = self.table.columnCount()
@@ -78,20 +91,60 @@ class Table(QWidget):
                     except Exception as e:
                         log(1, f"Error in DELETE ROW TABLE: {e}")
 
-    def add_row(self, item):
-        name, _, color, velocity = item
-        color_qt = QColor.fromRgb(color[2], color[1], color[0])
-        log(2, f"Add row: {name}, {velocity}, {color}")
+    def add_row(self, rows):
+        r"""
+        Added row in the table.
 
-        current_row = self.table.rowCount()
-        item_color = QTableWidgetItem()
-        item_color.setBackground(color_qt)
+        :param rows: rows to add.
+        """
 
-        self.table.insertRow(current_row)
-        self.table.setItem(current_row, 0, QTableWidgetItem(name))
-        self.table.setItem(current_row, 1, QTableWidgetItem(f"{velocity} km/h"))
-        self.table.setItem(current_row, 2, item_color)
-        self.table.update()
+        for item in rows:
+            if not self.check_cell(COLUMN_VEHICLE, item):
+                name, _, color, velocity = item
+                color_qt = QColor.fromRgb(color[2], color[1], color[0])
+                log(2, f"Add row: {name}, {velocity}, {color}")
+
+                current_row = self.table.rowCount()
+                item_color = QTableWidgetItem()
+                item_color.setBackground(color_qt)
+
+                self.table.insertRow(current_row)
+                self.table.setItem(current_row, 0, QTableWidgetItem(name))
+                self.table.setItem(current_row, 1, QTableWidgetItem(f"{velocity} km/h"))
+                self.table.setItem(current_row, 2, item_color)
+                self.table.update()
+
+    def check_cell(self, name_column, name_cell):
+        r"""
+        Check if name of the cell is present in the table.
+
+        :param name_column: column name.
+        :param name_cell: text to search into cell of the table.
+
+        :return: True if text is present, false otherwise.
+        """
+
+        columns = self.table.columnCount()
+        rows = self.table.rowCount()
+
+        is_present = False
+
+        for column in range(columns):
+            column_text = self.table.horizontalHeaderItem(column).text()
+
+            if column_text == name_column:
+
+                for row in range(rows):
+                    try:
+                        cell = self.table.item(row, column).text()
+
+                        if cell == name_cell:
+                            is_present = True
+                            break
+                    except Exception as e:
+                        log(1, f"Error in CHECK CELL: {e}")
+
+        return is_present
 
     @pyqtSlot()
     def on_click(self):
