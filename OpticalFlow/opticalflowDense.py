c2b6152482125c643d3dc5386401facd4fb8fb66
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-10-08 16:18:49 +0200

    Improved filter usage

--- OpticalFlow/opticalflowDense.py
+++ OpticalFlow/opticalflowDense.py
@@ -25,6 +25,13 @@ def callback_mouse(event, x, y, flag, param):
         print(f"Click image in position ({x},{y})")
 
 
+def filtering(frame):
+    r"""
+    Filtering image.
+    """
+    return cv.GaussianBlur(frame, ksize=(9, 9), sigmaX=cv.BORDER_DEFAULT)
+
+
 class OpticalFlowDense:
     r"""
     Class to detect moving vehicles by implementation of optical flow dense.
@@ -105,12 +112,17 @@ class OpticalFlowDense:
         self.table.show()
 
         _, first_frame = self.camera.read()
+
+        first_frame_copy = cv.resize(first_frame.copy(), (self.width, self.height))
+        first_frame_copy = cv.undistort(first_frame_copy, camera_matrix, dist_matrix, None, newcameramtx)
+
+        first_frame = filtering(first_frame)
         first_frame = cv.resize(first_frame, (self.width, self.height))
         first_frame = cv.undistort(first_frame, camera_matrix, dist_matrix, None, newcameramtx)
 
         prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
 
-        stack = Utility.stack_images(1, ([first_frame, first_frame]))
+        stack = Utility.stack_images(1, ([first_frame_copy, first_frame_copy]))
         cv.imshow(WINDOW_OPTICAL_FLOW, stack)
 
         mask = cv.resize(np.zeros_like(first_frame), (400, 400))
@@ -138,11 +150,14 @@ class OpticalFlowDense:
                     if self.show_log:
                         log(0, f"Iteration: {self.iterations}")
 
-                    frame = cv.medianBlur(frame, ksize=5)
+                    frame_copy = cv.resize(frame.copy(), (self.width, self.height))
+                    frame_copy = cv.undistort(frame_copy, camera_matrix, dist_matrix, None, newcameramtx)
+                    img_to_draw = frame_copy.copy()
+
+                    frame = filtering(frame)
                     frame = cv.resize(frame, (self.width, self.height))
                     frame = cv.undistort(frame, camera_matrix, dist_matrix, None, newcameramtx)
 
-                    frame_copy = frame.copy()
                     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
                     if self.excluded_area:
@@ -151,9 +166,9 @@ class OpticalFlowDense:
                         #    cv.polylines(frame, [polygon], True, (255, 0, 255), 8)
 
                     if self.excluded_area:
-                        frame = cv.addWeighted(frame_copy, self.alpha, frame, 1 - self.alpha, 0)
+                        img_to_draw = cv.addWeighted(img_to_draw, self.alpha, frame, 1 - self.alpha, 0)
 
-                    self.frame_rate(frame)
+                    self.frame_rate(img_to_draw)
 
                     # Optical Flow Dense
                     flow = cv.calcOpticalFlowFarneback(prev_gray, gray, None, pyr_scale=0.5, levels=5, winsize=11,
@@ -162,10 +177,11 @@ class OpticalFlowDense:
                     if self.show_log:
                         Utility.set_text(frame, f"Iter: {self.iterations}", (40, 45), dim=1.5, color=Color.RED)
 
-                    self.motion.detect_vehicle(img=frame, flow=flow, iter=self.iterations, fps=self.fps,
+                    self.motion.detect_vehicle(img=frame, img_to_draw=img_to_draw, flow=flow, iter=self.iterations,
+                                               fps=self.fps,
                                                polygons=self.polygons)
 
-                    stack = Utility.stack_images(1, ([frame, frame_copy]))
+                    stack = Utility.stack_images(1, ([img_to_draw, frame_copy]))
                     cv.imshow(WINDOW_OPTICAL_FLOW, stack)
 
                     key = cv.waitKey(1)
